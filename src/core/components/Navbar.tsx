4a04baf0967dd29b216b682db407b72a5c083b92
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-27 04:27:11 +0900

    feat: add navbar

---
+++ src/core/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Navbar as FlowbiteNavbar, Button } from "flowbite-react"
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <FlowbiteNavbar fluid={true} id="navbar">
+      <Link href="/">
+        <div className="flex">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            WeWrite
+          </span>
+        </div>
+      </Link>
+      <FlowbiteNavbar.Collapse>
+        <FlowbiteNavbar.Link href="#" active={true}>
+          Home
+        </FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">About</FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">Services</FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">Pricing</FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">Contact</FlowbiteNavbar.Link>
+      </FlowbiteNavbar.Collapse>
+      <div className="flex md:order-2 gap-4">
+        <Button color="gray">Login</Button>
+        <Button color="gray">Register</Button>
+        <FlowbiteNavbar.Toggle />
+      </div>
+    </FlowbiteNavbar>
+  )
+}
+
+export default Navbar
--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import React, {FC} from "react"
 import {BlitzLayout} from "@blitzjs/next"
+import Navbar from "../components/Navbar";
 
 const Layout: BlitzLayout<{title?: string; children?: React.ReactNode}> = ({title, children}) => {
   return (
@@ -10,6 +11,7 @@ const Layout: BlitzLayout<{title?: string; children?: React.ReactNode}> = ({titl
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar />
       {children}
     </>
   )
