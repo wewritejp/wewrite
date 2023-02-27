db94001fbdba3c7486d5837381394d7184a0d8df
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-28 02:27:10 +0900

    feat: validate book update

--- src/books/mutations/updateBook.ts
+++ src/books/mutations/updateBook.ts
@@ -5,9 +5,13 @@ import { UpdateBook } from "../validations"
 export default resolver.pipe(
   resolver.zod(UpdateBook),
   resolver.authorize(),
-  async ({ id, ...data }) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const book = await db.book.update({ where: { id }, data })
+  async ({ id, ...data }, ctx) => {
+    const currentUserId = ctx.session.userId
+    const book = await db.book.findFirst({ where: { id } })
+
+    if (!book || book.userId !== currentUserId) return
+
+    await db.book.update({ where: { id }, data })
 
     return book
   }
