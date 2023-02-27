b6b62be572ba65d13af0ebb618d7c76ffa83f725
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-27 18:42:33 +0900

    fix: validations.ts

--- src/books/mutations/createBook.ts
+++ src/books/mutations/createBook.ts
@@ -1,6 +1,6 @@
 import { resolver } from "@blitzjs/rpc"
 import db from "db"
-import { CreateBook } from "../validation"
+import { CreateBook } from "../validations"
 
 export default resolver.pipe(resolver.zod(CreateBook), resolver.authorize(), async (input) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
