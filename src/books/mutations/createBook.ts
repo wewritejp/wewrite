cd4900d118003c7e2af2f97179562683aa12da8e
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-27 23:00:18 +0900

    fix: create book mutations to reister user

--- src/books/mutations/createBook.ts
+++ src/books/mutations/createBook.ts
@@ -2,9 +2,13 @@ import { resolver } from "@blitzjs/rpc"
 import db from "db"
 import { CreateBook } from "../validations"
 
-export default resolver.pipe(resolver.zod(CreateBook), resolver.authorize(), async (input) => {
-  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const book = await db.book.create({ data: input })
+export default resolver.pipe(resolver.zod(CreateBook), resolver.authorize(), async (input, ctx) => {
+  const currentUser = ctx.session.userId
+  const data = {
+    ...input,
+    userId: currentUser,
+  }
+  const book = await db.book.create({ data })
 
   return book
 })
