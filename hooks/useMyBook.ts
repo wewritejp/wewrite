e9013eb496fb9d00b9c946fa766e9f4f60772d77
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-28 07:30:08 +0900

    feat: create useMyBook hooks & display book edit button

---
+++ hooks/useMyBook.ts
@@ -0,0 +1,16 @@
+import { Book } from "@prisma/client"
+import { useEffect, useState } from "react"
+import { useSession } from "@blitzjs/auth"
+
+export const useMyBook = (book: Book) => {
+  const [isMyBook, setIsMyBook] = useState(false)
+  const session = useSession({ suspense: false })
+
+  useEffect(() => {
+    setIsMyBook(book.userId == session.userId)
+  }, [book, session])
+
+  return { isMyBook }
+}
+
+export default useMyBook
