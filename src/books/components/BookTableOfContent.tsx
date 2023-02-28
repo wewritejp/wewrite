afe9d4b54332b61f96b36efc8713deefb3104cb4
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-28 19:35:30 +0900

    style: add NewSectionPage button in BookTableOfContent

--- src/books/components/BookTableOfContent.tsx
+++ src/books/components/BookTableOfContent.tsx
@@ -1,4 +1,4 @@
-import { Book } from "@prisma/client"
+import { Book, Section } from "@prisma/client"
 import { Button } from "flowbite-react"
 import Link from "next/link"
 import { FC } from "react"
@@ -10,9 +10,10 @@ import deleteChapter from "src/chapters/mutations/deleteChapter"
 import { useRouter } from "next/router"
 import { BsPencil } from "react-icons/bs"
 import { Routes } from "@blitzjs/next"
+import { HiPlus } from 'react-icons/hi'
 
 type Props = {
-  book: Book & { chapters: Chapter[] }
+  book: Book & { chapters: Chapter[] & { sections: Section[] } }
 }
 
 const BookTableOfContent: FC<Props> = ({ book }) => {
@@ -65,18 +66,25 @@ const BookTableOfContent: FC<Props> = ({ book }) => {
       <>
         {!!book.chapters[0] ? (
           <>
-            {book.chapters.map((chapter, index) => (
-              <div className="flex flex-col gap-2" key={index}>
+            {book.chapters.map((chapter: Chapter & { sections: Section[] }, chapterIndex) => (
+              <div className="flex flex-col gap-2" key={chapterIndex}>
                 <div className="flex pb-4">
-                  <h3 className="text-blue-800 w-28 my-auto">Chapter {index + 1}</h3>
+                  <h3 className="text-blue-800 w-28 my-auto">Chapter {chapterIndex + 1}</h3>
                   <h4 className="my-auto">{chapter.headline}</h4>
                   {isMyBook && (
                     <div className="ml-auto flex gap-2">
                       <Link
+                        href={Routes.NewSectionPage({ bookId: book.id, chapterId: chapter.id })}
+                      >
+                        <Button size={"xs"}>
+                          <HiPlus className="h-4 w-4 mx-1" />
+                        </Button>
+                      </Link>
+                      <Link
                         href={Routes.EditChapterPage({ bookId: book.id, chapterId: chapter.id })}
                       >
                         <Button color="success" size={"xs"}>
-                          <BsPencil className="h-4 w-4 mx-2" />
+                          <BsPencil className="h-4 w-4 mx-1" />
                         </Button>
                       </Link>
                       <Button
@@ -84,15 +92,19 @@ const BookTableOfContent: FC<Props> = ({ book }) => {
                         size={"xs"}
                         onClick={() => handleDeleteChapter(chapter.id)}
                       >
-                        <MdDelete className="h-4 w-4 mx-2" />
+                        <MdDelete className="h-4 w-4 mx-1" />
                       </Button>
                     </div>
                   )}
                 </div>
                 {/* <div className="border-b pb-4 mb-2 text-black">
             0-1 TypeScript と React で Unsplash 風アプリを作ろう
-          </div>
-          <div className="border-b pb-4 mb-2 text-black">0-2 VSCode のインストール</div> */}
+          </div> */}
+                {chapter.sections.map((section, sectionIndex) => (
+                  <div className="border-b pb-4 mb-2 text-black" key={sectionIndex}>
+                    0-2 VSCode のインストール
+                  </div>
+                ))}
               </div>
             ))}
           </>
