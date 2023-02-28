3fbb44409935eacabf6472f82af6ef6111f46c07
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-28 15:52:05 +0900

    feat: gssr in EditChapterPage

--- src/chapters/mutations/updateChapter.ts
+++ src/chapters/mutations/updateChapter.ts
@@ -1,19 +1,19 @@
-import { resolver } from "@blitzjs/rpc";
-import db from "db";
-import { z } from "zod";
+import { resolver } from "@blitzjs/rpc"
+import db from "db"
+import { z } from "zod"
 
 const UpdateChapter = z.object({
-  id: z.number(),
-  name: z.string(),
-});
+  id: z.string(),
+  headline: z.string(),
+})
 
 export default resolver.pipe(
   resolver.zod(UpdateChapter),
   resolver.authorize(),
   async ({ id, ...data }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const chapter = await db.chapter.update({ where: { id }, data });
+    const chapter = await db.chapter.update({ where: { id }, data })
 
-    return chapter;
+    return chapter
   }
-);
+)
