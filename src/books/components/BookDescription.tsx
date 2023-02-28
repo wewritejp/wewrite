636dd4aadaf21db0e758951598c11f3934ff7b63
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-28 09:50:38 +0900

    feat: create BookAuthorProfile

--- src/books/components/BookDescription.tsx
+++ src/books/components/BookDescription.tsx
@@ -1,39 +1,28 @@
 import { Avatar } from "flowbite-react"
 import BookTableOfContent from "./BookTableOfContent"
+import BookAuthorProfile from "./BookAuthorProfile"
 
 const BookDescription = ({ book }) => {
+  const items = [
+    { headline: book.purpose, body: <div className="sentence">{book.content}</div> },
+    { headline: "Note", body: <div className="sentence">{book.note}</div> },
+    { headline: "Table Of Content", body: <BookTableOfContent book={book} /> },
+    {
+      headline: "Author",
+      body: <BookAuthorProfile user={book.user} />,
+    },
+  ]
+
   return (
     <section className="max-w-4xl mx-auto py-8 px-4">
       <div className="md:grid md:grid-cols-12">
         <div className="md:col-span-12 format">
-          <article>
-            <h3 className="headline">{book.purpose}</h3>
-            <div className="sentence">
-              {book.content}
-            </div>
-          </article>
-          <article>
-            <h3 className="headline">Note</h3>
-            <div className="sentence">
-              {book.note}
-            </div>
-          </article>
-          <article>
-            <h3 className="headline">Table Of Content</h3>
-            <BookTableOfContent book={book} />
-          </article>
-          <article>
-            <h3 className="headline">Author</h3>
-            <div className="flex gap-4 p-2">
-              <div className="flex flex-col gap-2">
-                <Avatar size={"xl"} rounded />
-                <h4 className="mx-auto">{book.user.name}</h4>
-              </div>
-              <p className="my-auto">
-                現在は教育業界でサーバーサイド・フロントエンドエンジニアとして働きながら、休日は個人サービスの開発を楽しんでいます。RubyとVimがとっても好きです。Twitterアカウント：@rails_java_like
-              </p>
-            </div>
-          </article>
+          {items.map((item) => (
+            <article>
+              <h3 className="headline">{item.headline}</h3>
+              {item.body}
+            </article>
+          ))}
         </div>
       </div>
     </section>
