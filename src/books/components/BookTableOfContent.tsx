5817062b9fa7c64fd274b473cee169484bb3212a
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-01 18:01:51 +0900

    feat: add Visit Button in BookTableOfContent

--- src/books/components/BookTableOfContent.tsx
+++ src/books/components/BookTableOfContent.tsx
@@ -11,6 +11,9 @@ import {
   EditSectionButton,
   VisitSectionButton,
 } from "./BookTableOfContentButtons"
+import { Button } from "flowbite-react"
+import Link from "next/link"
+import { Routes } from "@blitzjs/next"
 
 type Props = {
   book: Book & { chapters: Chapter[] & { sections: Section[] } }
@@ -21,42 +24,55 @@ const BookTableOfContent: FC<Props> = ({ book }) => {
 
   return (
     <div className="flex flex-col gap-8 py-2">
-      <>
-        {!!book.chapters[0] ? (
-          <>
-            {book.chapters.map((chapter: Chapter & { sections: Section[] }, chapterIndex) => (
-              <div className="flex flex-col gap-2" key={chapterIndex}>
-                <div className="flex pb-4">
-                  <h3 className="text-blue-800 w-28 my-auto">Chapter {chapterIndex + 1}</h3>
-                  <h4 className="my-auto">{chapter.headline}</h4>
-                  {isMyBook && (
-                    <div className="ml-auto flex gap-2">
-                      <AddNewSectionButton book={book} chapter={chapter} />
-                      <EditChapterButton book={book} chapter={chapter} />
-                      <DeleteChapterButton book={book} chapter={chapter} />
-                    </div>
-                  )}
-                </div>
-                {chapter.sections.map((section, sectionIndex) => (
-                  <div className="border-b pb-4 mb-2 text-black flex">
-                    <span>
-                      {chapterIndex + 1}-{sectionIndex + 1} {section.title}
-                    </span>
-
-                    <div className="ml-auto flex gap-1">
-                      <VisitSectionButton book={book} chapter={chapter} section={section} />
-                      <EditSectionButton book={book} chapter={chapter} section={section} />
-                      <DeleteSectionButton book={book} chapter={chapter} section={section} />
-                    </div>
+      {!!book.chapters[0] ? (
+        <>
+          {book.chapters.map((chapter: Chapter & { sections: Section[] }, chapterIndex) => (
+            <div className="flex flex-col gap-2" key={chapterIndex}>
+              <div className="flex pb-4">
+                <h3 className="text-blue-800 w-28 my-auto">Chapter {chapterIndex + 1}</h3>
+                <h4 className="my-auto">{chapter.headline}</h4>
+                {isMyBook && (
+                  <div className="ml-auto flex gap-2">
+                    <AddNewSectionButton book={book} chapter={chapter} />
+                    <EditChapterButton book={book} chapter={chapter} />
+                    <DeleteChapterButton book={book} chapter={chapter} />
                   </div>
-                ))}
+                )}
               </div>
-            ))}
-          </>
-        ) : (
-          <p>There is no chapters yet.</p>
-        )}
-      </>
+              {chapter.sections.map((section, sectionIndex) => (
+                <div className="border-b pb-4 mb-2 text-black flex">
+                  <span>
+                    {chapterIndex + 1}-{sectionIndex + 1} {section.title}
+                  </span>
+                  <div className="ml-auto flex gap-1">
+                    {isMyBook ? (
+                      <>
+                        <VisitSectionButton book={book} chapter={chapter} section={section} />
+                        <EditSectionButton book={book} chapter={chapter} section={section} />
+                        <DeleteSectionButton book={book} chapter={chapter} section={section} />
+                      </>
+                    ) : (
+                      <Link
+                        href={Routes.ShowSectionPage({
+                          bookId: book.id,
+                          chapterId: chapter.id,
+                          sectionId: section.id,
+                        })}
+                      >
+                        <Button size={"xs"} className="px-4">
+                          Visit
+                        </Button>
+                      </Link>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>There is no chapters yet.</p>
+      )}
 
       {isMyBook && <AddChapterButton book={book} />}
     </div>
