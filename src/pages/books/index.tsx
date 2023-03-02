d3e56d6931406ab1287af406c010888e00d94660
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-02 19:00:33 +0900

    feat: dynamic BookCard in BooksPage

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,6 +1,7 @@
 import { BlitzPage } from "@blitzjs/next"
 import { Book } from "@prisma/client"
 import { gSSP } from "src/blitz-server"
+import BookCard from "src/books/components/BookCard"
 import getBooks from "src/books/queries/getBooks"
 import Layout from "src/core/layouts/Layout"
 
@@ -14,15 +15,15 @@ export const getServerSideProps = gSSP(async ({ ctx }) => {
 })
 
 const BooksPage: BlitzPage<Props> = ({ books }) => {
-  console.log(books)
   return (
     <Layout>
-      <div>
-        {books.map((book, bookIndex) => (
-          <div key={bookIndex}>
-            <p>{book.title}</p>
-          </div>
-        ))}
+      <div className="max-w-5xl mx-auto flex flex-col gap-8 py-10 px-4">
+        <h1 className="text-blue-700 font-bold text-3xl">Articles</h1>
+        <div className="grid md:grid-cols-3 gap-4">
+          {books.map((book, bookIndex) => (
+            <BookCard book={book} key={bookIndex} />
+          ))}
+        </div>
       </div>
     </Layout>
   )
