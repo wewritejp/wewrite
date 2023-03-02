84b90502f495033d5d6db933fd6d5c09451c1b39
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-02 19:00:33 +0900

    feat: dynamic BookCard in BooksPage

--- src/books/components/BookCard.tsx
+++ src/books/components/BookCard.tsx
@@ -7,10 +7,10 @@ const BookCard = ({ book }) => {
     <Link href={Routes.ShowBookPage({ bookId: book.id })}>
       <a>
         <Card imgSrc="https://i.ytimg.com/vi/-UjD_YrVhUM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-Jl1S1fLGAmlaj_7Jg8oE7dXCmw">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="font-bold tracking-tight text-gray-900 dark:text-white">
             {book.title}
           </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-3">{book.body}</p>
+          <p className="font-normal text-gray-700 dark:text-gray-400 line-clamp-4 text-sm">{book.body}</p>
         </Card>
       </a>
     </Link>
