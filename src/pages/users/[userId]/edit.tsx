bba486efce01e33fc069d5e4901e15880d47be84
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-04 10:01:12 +0900

    feat: getUser in EditUserPage by ssr

--- src/pages/users/[userId]/edit.tsx
+++ src/pages/users/[userId]/edit.tsx
@@ -1,84 +1,28 @@
-import { Suspense } from "react";
-import { Routes } from "@blitzjs/next";
-import Head from "next/head";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useQuery, useMutation } from "@blitzjs/rpc";
-import { useParam } from "@blitzjs/next";
+import { BlitzPage } from "@blitzjs/next"
+import { User } from "@prisma/client"
+import { gSSP } from "src/blitz-server"
+import getUser from "src/users/queries/getUser"
 
-import Layout from "src/core/layouts/Layout";
-import getUser from "src/users/queries/getUser";
-import updateUser from "src/users/mutations/updateUser";
-import { UserForm, FORM_ERROR } from "src/users/components/UserForm";
+type Props = {
+  user: User
+}
 
-export const EditUser = () => {
-  const router = useRouter();
-  const userId = useParam("userId", "string");
-  const [user, { setQueryData }] = useQuery(
-    getUser,
-    { id: userId },
-    {
-      // This ensures the query never refreshes and overwrites the form data while the user is editing.
-      staleTime: Infinity,
-    }
-  );
-  const [updateUserMutation] = useMutation(updateUser);
+export const getServerSideProps = gSSP(async ({ query, ctx }) => {
+  const id = query.userId as string
+  const user = await getUser({ id }, ctx)
+  const isMe = ctx.session.userId == id
 
-  return (
-    <>
-      <Head>
-        <title>Edit User {user.id}</title>
-      </Head>
+  return { props: { user }, notFound: !isMe }
+})
 
-      <div>
-        <h1>Edit User {user.id}</h1>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-
-        <UserForm
-          submitText="Update User"
-          // TODO use a zod schema for form validation
-          //  - Tip: extract mutation's schema into a shared `validations.ts` file and
-          //         then import and use it here
-          // schema={UpdateUser}
-          initialValues={user}
-          onSubmit={async (values) => {
-            try {
-              const updated = await updateUserMutation({
-                id: user.id,
-                ...values,
-              });
-              await setQueryData(updated);
-              await router.push(Routes.ShowUserPage({ userId: updated.id }));
-            } catch (error: any) {
-              console.error(error);
-              return {
-                [FORM_ERROR]: error.toString(),
-              };
-            }
-          }}
-        />
-      </div>
-    </>
-  );
-};
-
-const EditUserPage = () => {
+const EditUserPage: BlitzPage<Props> = ({ user }) => {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <EditUser />
-      </Suspense>
-
-      {/* <p>
-        <Link href={Routes.UsersPage()}>
-          <a>Users</a>
-        </Link>
-      </p> */}
+      <p>{user.name}</p>
     </div>
-  );
-};
+  )
+}
 
-EditUserPage.authenticate = true;
-EditUserPage.getLayout = (page) => <Layout>{page}</Layout>;
+EditUserPage.authenticate = true
 
-export default EditUserPage;
+export default EditUserPage
