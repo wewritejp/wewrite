7db863106b764b39958804e23de73b74dd59360d
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-04 09:49:44 +0900

    feat: add EditUserPage link in Navbar

--- src/users/queries/getUser.ts
+++ src/users/queries/getUser.ts
@@ -5,12 +5,11 @@ import { z } from "zod";
 
 const GetUser = z.object({
   // This accepts type of undefined, but is required at runtime
-  id: z.number().optional().refine(Boolean, "Required"),
+  id: z.string().optional().refine(Boolean, "Required"),
 });
 
 export default resolver.pipe(
   resolver.zod(GetUser),
-  resolver.authorize(),
   async ({ id }) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     const user = await db.user.findFirst({ where: { id } });
