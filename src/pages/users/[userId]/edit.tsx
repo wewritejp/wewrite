cfba41cb93ff1ac9b0dafd94210fbb42a0b840d8
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-04 10:42:49 +0900

    style: EditUserPage

--- src/pages/users/[userId]/edit.tsx
+++ src/pages/users/[userId]/edit.tsx
@@ -1,6 +1,11 @@
 import { BlitzPage } from "@blitzjs/next"
+import { useMutation } from "@blitzjs/rpc"
 import { User } from "@prisma/client"
 import { gSSP } from "src/blitz-server"
+import Footer from "src/core/components/Footer"
+import Layout from "src/core/layouts/Layout"
+import { FORM_ERROR, UserForm } from "src/users/components/UserForm"
+import updateUser from "src/users/mutations/updateUser"
 import getUser from "src/users/queries/getUser"
 
 type Props = {
@@ -16,10 +21,30 @@ export const getServerSideProps = gSSP(async ({ query, ctx }) => {
 })
 
 const EditUserPage: BlitzPage<Props> = ({ user }) => {
+  const [updateUserMutation] = useMutation(updateUser)
   return (
-    <div>
-      <p>{user.name}</p>
-    </div>
+    <Layout title={user.name}>
+      <div className="max-w-2xl mx-auto flex flex-col gap-4 p-4">
+        <h1 className="text-2xl font-semibold">Edit ‘‘{user.name}‘‘</h1>
+
+        <UserForm
+          submitText="Update Profile"
+          // schema={UpdateUser}
+          initialValues={user}
+          onSubmit={async (values) => {
+            try {
+              await updateUserMutation({ ...values })
+            } catch (error: any) {
+              console.log(error)
+              return {
+                [FORM_ERROR]: error.toString(),
+              }
+            }
+          }}
+        />
+      </div>
+      <Footer />
+    </Layout>
   )
 }
 
