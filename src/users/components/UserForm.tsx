675a74cf9d6db04887af34a4b4461a5d538fc4b2
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-04 10:42:49 +0900

    style: EditUserPage

--- src/users/components/UserForm.tsx
+++ src/users/components/UserForm.tsx
@@ -1,12 +1,15 @@
-import { Form, FormProps } from "src/core/components/Form";
-import { LabeledTextField } from "src/core/components/LabeledTextField";
-import { z } from "zod";
-export { FORM_ERROR } from "src/core/components/Form";
+import { Form, FormProps } from "src/core/components/Form"
+import { LabeledTextField } from "src/core/components/LabeledTextField"
+import LabeledTextarea from "src/core/components/LabeledTextarea"
+import { z } from "zod"
+export { FORM_ERROR } from "src/core/components/Form"
 
 export function UserForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
       <LabeledTextField name="name" label="Name" placeholder="Name" />
+      <LabeledTextField name="imageUrl" label="Image URL" placeholder="Image URL" />
+      <LabeledTextarea name="introduction" label="Introduction" placeholder="Introduction" rows={20} />
     </Form>
-  );
+  )
 }
