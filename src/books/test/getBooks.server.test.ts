bcccc629b8b2cf36124f15cf134f0a6b3f7dda91
hulk-higakijin <hm385.chejptks@gmail.com>
2023-03-15 02:00:21 +0900

    feat: add test for checking draft book in getBooks

--- src/books/test/getBooks.server.test.ts
+++ src/books/test/getBooks.server.test.ts
@@ -19,17 +19,22 @@ describe("Test getBooks", () => {
   describe("when filter 'published' as search condition", () => {
     beforeEach(async () => {
       await db.user.create({ data: dummyUser })
-      await db.book.create({ data: dummyBookA })
     })
 
     it("return all published books", async () => {
+      await db.book.create({ data: dummyBookA })
+
       const { books } = await getBooks({ where: { isPublished: true } }, mockCtx)
       expect(books[0]?.title).toEqual(dummyBookA.title)
       expect(books[0]?.isPublished).toEqual(true)
       expect(books[0]?.user).toEqual(dummyUser)
     })
 
-    it('does not return draft books', async () => {
+    it("does not return draft books", async () => {
+      await db.book.create({ data: { ...dummyBookA, isPublished: false } })
+
+      const { books } = await getBooks({ where: { isPublished: true } }, mockCtx)
+      expect(books).toEqual([])
     })
   })
 })
